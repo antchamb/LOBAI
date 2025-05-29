correcting ofi computation

LOBAPP/data_loader/FI2010.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import torch
 from lean.models import file_path
+from tqdm import tqdm
+from matplotlib.rcsetup import validate_backend
 
 
 def __get_raw__(auction, normalization, day):
@@ -261,26 +263,45 @@ class Dataset_fi2010:
 
         rows_per_level, base, ask_row, bid_row, max_level = self.handling_level(level, self.T, self.lighten)
         print(self.x.squeeze().shape)
-        # Extract ask and bid depths
 
-        ask_depth = self.x.squeeze()[:, ask_row]
-        bid_depth = self.x.squeeze()[:, bid_row]
+        snap = self.x[:, 0, 0, :].cpu().numpy()
 
-        # Compute differences
-        d_ask = np.diff(ask_depth, axis=0, prepend=ask_depth[0:1])
-        d_bid = np.diff(bid_depth, axis=0, prepend=bid_depth[0:1])
+        n = snap.shape[0]
+        ofi = np.zeros(n, dtype=np.float64)
 
-        # Compute OFI
-        ofi_series = d_bid - d_ask
+        lvl_list = list(range(1, max_level + 1))
 
-        return ofi_series
-        # elif agg == "mean":
-        #     return np.array(ofi_series.mean())
-        # elif agg == "median":
-        #     return np.array(np.median(ofi_series, axis=0))
-        # else:
-        #     raise ValueError("Invalid aggregation method. Use 'series', 'mean', or 'median'.")
-        #
+        for lvl in tqdm(lvl_list):
+            base = 4 * (lvl - 1)
+            pa = snap[:, base]
+            va = snap[:, base + 1]
+            pb = snap[:, base + 2]
+            vb = snap[:, base + 3]
+
+            # bid order flow
+            bOF = np.zeros_like(pb)
+            diff_b = pb[1:] - pb[:-1]
+            mask_up = diff_b > 0
+            mask_same = diff_b == 0
+            mask_down = diff_b < 0
+            bOF[1:][mask_up] = vb[1:][mask_up]
+            bOF[1:][mask_same] = vb[1:][mask_same] - vb[:-1][mask_same]
+            bOF[1:][mask_down] = -vb[1:][mask_down]
+
+            # ask order flow
+            aOF = np.zeros_like(pa)
+            diff_a = pa[1:] - pa[:-1]
+            mask_up = diff_a > 0
+            mask_same = diff_a == 0
+            mask_down = diff_a < 0
+            aOF[1:][mask_up] = -va[:-1][mask_up]
+            aOF[1:][mask_same] = va[1:][mask_same] - va[:-1][mask_same]
+            aOF[1:][mask_down] = va[1:][mask_down]
+
+        ofi += bOF + aOF
+
+        return ofi
+
     def as_2d(self):
         """
         Returns the dataset as a 2D numpy array.

LOBAPP/debug_test.py
@@ -1,23 +1,23 @@
-# import numpy as np
-#
-# from data_loader.FI2010 import Dataset_fi2010
-#
-# stocks= [0]
-#
-# days = [1,2]
-# norm="Zscore"
-# graphs = []
-#
-# ds = Dataset_fi2010(
-#     auction=False,
-#     normalization=norm,
-#     stock_idx=stocks,
-#     days=days,
-#     T=1,
-#     k=0,  # k is not used in OFI calculation
-#     lighten=True,
-# )
-#
-# a, b, c =ds.get_ofi()
+import numpy as np
+
+from data_loader.FI2010 import Dataset_fi2010
+
+stocks= [0]
+
+days = [1,2]
+norm="Zscore"
+graphs = []
+
+ds = Dataset_fi2010(
+    auction=False,
+    normalization=norm,
+    stock_idx=stocks,
+    days=days,
+    T=1,
+    k=0,  # k is not used in OFI calculation
+    lighten=True,
+)
+
+ofi =ds.get_ofi()
 
 # tick_time = np.arange(len(ofi_series))

LOBAPP/models/deeplob.py
@@ -129,42 +129,42 @@ class Deeplob(nn.Module):
     #     return forecast_y
 
     def forward(self, x):
-        print(f"Input shape: {x.shape}")  # Initial input shape
+        # print(f"Input shape: {x.shape}")  # Initial input shape
 
         batch_size = x.size(0)
         h0 = torch.zeros(1, batch_size, 64, device=x.device)
         c0 = torch.zeros(1, batch_size, 64, device=x.device)
 
         x = self.conv1(x)
-        print(f"After conv1: {x.shape}")
+        # print(f"After conv1: {x.shape}")
 
         x = self.conv2(x)
-        print(f"After conv2: {x.shape}")
+        # print(f"After conv2: {x.shape}")
 
         x = self.conv3(x)
-        print(f"After conv3: {x.shape}")
+        # print(f"After conv3: {x.shape}")
 
         x_inp1 = self.inp1(x)
         x_inp2 = self.inp2(x)
         x_inp3 = self.inp3(x)
-        print(f"After inp1: {x_inp1.shape}, inp2: {x_inp2.shape}, inp3: {x_inp3.shape}")
+        # print(f"After inp1: {x_inp1.shape}, inp2: {x_inp2.shape}, inp3: {x_inp3.shape}")
 
         x = torch.cat((x_inp1, x_inp2, x_inp3), dim=1)
-        print(f"After concatenation: {x.shape}")
+        # print(f"After concatenation: {x.shape}")
 
         x = x.permute(0, 2, 1, 3)  # (B, T, C, D)
-        print(f"After permute: {x.shape}")
+        # print(f"After permute: {x.shape}")
 
         x = x.mean(dim=-1, keepdim=False)
 
         x = torch.reshape(x, (batch_size, x.shape[1], -1))  # (B, T, C*D)
-        print(f"After reshape: {x.shape}")
+        # print(f"After reshape: {x.shape}")
 
         if x.shape[-1] != 192:  # Debugging check
             raise ValueError(f"Expected input size 192, but got {x.shape[-1]}")
 
         x, _ = self.lstm(x, (h0, c0))
-        print(f"After LSTM: {x.shape}")
+        # print(f"After LSTM: {x.shape}")
 
         x = x[:, -1, :]
         x = self.fc1(x)

LOBAPP/utils/load_weights.py
@@ -3,6 +3,8 @@ from models.deeplob import Deeplob
 from pathlib import Path
 from data_loader.FI2010 import Dataset_fi2010
 import random
+import tqdm as tqdm
+import time
 
 CKPT = Path(__file__).resolve().parents[1] / "weights" / "deeplob_light.pt"
 
@@ -61,18 +63,28 @@ ds = Dataset_fi2010(
     k=0,
     lighten=True,
 )
-loader = torch.utils.data.DataLoader(ds, batch_size=1024)
+loader = torch.utils.data.DataLoader(ds, batch_size=1)
+
+
+timing = []
 all_pred, all_true =  [], []
 with torch.no_grad():
     for xb, yb in loader:
         xb, yb = xb.to(device), yb.to(device)
+        torch.cuda.empty_cache()
+        start = time.time()
+        net(xb.to(device))
+        torch.cuda.synchronize()
+        timing.append(time.time() - start)
         probs =  net(xb).argmax(1)
         all_pred.append(probs.cpu())
         all_true.append(yb.cpu())
 
 y_pred = torch.cat(all_pred).numpy()
 y_true = torch.cat(all_true).numpy()
+timing = torch.as_tensor(timing, dtype=torch.float32)
 
+print(round(timing.mean().item(), 4), 'ms/data')
 # Classification report
 print(classification_report(y_true, y_pred, target_names=["down", "flat", "up"]))
 